c488a750dc73a0f828e32f200f093ce883745a82
Jayan Chinthaka <mail4jayanchinthaka@gmail.com>
2023-10-11 18:41:24 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <navigation-drawer @selectOption="onSelectOption" />
+
+    <v-main>
+      <image-gallary :isWithColor="isWithColor" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import NavigationDrawer from "./components/NavigationDrawer.vue";
+import ImageGallary from "./components/ImageGallary.vue";
+
+const isWithColor = ref(true);
+
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor;
+};
+</script>
---
+++ src/components/ImageGallary.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card class="mx-5 my-2 pa-3">
+    <v-row>
+      <v-col v-for="n in 200" :key="n" cols="4" sm="3" md="2" lg="1">
+        <v-hover v-slot="{ isHovering, props }">
+          <v-card
+            v-bind="props"
+            :elevation="isHovering ? 12 : 2"
+            style="cursor: pointer"
+            @click="
+              copyUrl(
+                `https://picsum.photos/500/300?image=${n * 5 + 10}${
+                  isWithColor ? '' : '&grayscale'
+                }`
+              )
+            "
+          >
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}${
+                isWithColor ? '' : '&grayscale'
+              }`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}${
+                isWithColor ? '' : '&grayscale'
+              }`"
+              aspect-ratio="1"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps(["isWithColor"]);
+
+const copyUrl = async (url) => {
+  await navigator.clipboard.writeText(url);
+};
+</script>
